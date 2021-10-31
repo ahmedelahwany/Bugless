eb986cd0e903fb463b58964940033af83cfd413e
ahmedelahwany <ahmed.elahwany26@gmail.com>
2021-10-31 10:14:28 +0100

    Add a Form for sending mesaages and uploading media to the chat

---
+++ src/components/MainScreen/MessageForm.js
@@ -0,0 +1,262 @@
+import React from "react";
+import uuidv4 from "uuid/v4";
+import firebase from "../../firebase";
+import { Segment, Button, Input } from "semantic-ui-react";
+import { Picker, emojiIndex } from "emoji-mart";
+import "emoji-mart/css/emoji-mart.css";
+
+import FileModal from "./FileModal";
+import ProgressBar from "./ProgressBar";
+
+class MessageForm extends React.Component {
+  state = {
+    errors: [],
+    issue: this.props.currentIssue,
+    modal: false,
+    message: "",
+    uploadState: "",
+    uploadTask: null,
+    user: this.props.currentUser,
+    loading: false,
+    percentUploaded: 0,
+    storageRef: firebase.storage().ref(),
+    emojiPicker: false,
+  };
+
+  componentWillUnmount() {
+    if (this.state.uploadTask !== null) {
+      this.state.uploadTask.cancel();
+      this.setState({ uploadTask: null });
+    }
+  }
+
+  openModal = () => this.setState({ modal: true });
+
+  closeModal = () => this.setState({ modal: false });
+
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  handleKeyDown = (event) => {
+    if (event.ctrlKey && event.keyCode === 13) {
+      this.sendMessage();
+    }
+
+    const { message,issue, user } = this.state;
+
+   
+  };
+
+  handleTogglePicker = () => {
+    this.setState({ emojiPicker: !this.state.emojiPicker });
+  };
+
+  colonToUnicode = (message) => {
+    return message.replace(/:[A-Za-z0-9_+-]+:/g, (x) => {
+      x = x.replace(/:/g, "");
+      let emoji = emojiIndex.emojis[x];
+      if (typeof emoji !== "undefined") {
+        let unicode = emoji.native;
+        if (typeof unicode !== "undefined") {
+          return unicode;
+        }
+      }
+      x = ":" + x + ":";
+      return x;
+    });
+  };
+
+  handleAddEmoji = (emoji) => {
+    const oldMessage = this.state.message;
+    const newMessage = this.colonToUnicode(` ${oldMessage} ${emoji.colons} `);
+    this.setState({ message: newMessage, emojiPicker: false });
+    setTimeout(() => this.messageInputRef.focus(), 0);
+  };
+
+  sendMessage = () => {
+    const { getMessagesRef } = this.props;
+    const { message, issue, user} = this.state;
+
+    if (message) {
+      this.setState({ loading: true });
+      getMessagesRef()
+        .child(issue.id)
+        .push()
+        .set(this.createMessage())
+        .then(() => {
+          this.setState({ loading: false, message: "", errors: [] });
+        })
+        .catch((err) => {
+          console.error(err);
+          this.setState({
+            loading: false,
+            errors: this.state.errors.concat(err),
+          });
+        });
+    } else {
+      this.setState({
+        errors: this.state.errors.concat({ message: "Add a message" }),
+      });
+    }
+  };
+
+  createMessage = (fileUrl = null) => {
+    const message = {
+      timestamp: firebase.database.ServerValue.TIMESTAMP,
+      user: {
+        id: this.state.user.uid,
+        name: this.state.user.displayName,
+        avatar: this.state.user.photoURL,
+      },
+    };
+    if (fileUrl !== null) {
+      message["image"] = fileUrl;
+    } else {
+      message["content"] = this.state.message;
+    }
+    return message;
+  };
+  getPath = () => {
+    if (this.props.isPrivateIssue) {
+      return `chat/private/${this.state.issue.id}`;
+    } else {
+      return "chat/public";
+    }
+  };
+
+  uploadFile = (file, metadata) => {
+    const pathToUpload = this.state.issue.id;
+    const ref = this.props.getMessagesRef();
+    const filePath = `${this.getPath()}/${uuidv4()}.jpg`;
+
+    this.setState(
+      {
+        uploadState: "uploading",
+        uploadTask: this.state.storageRef.child(filePath).put(file, metadata),
+      },
+      () => {
+        this.state.uploadTask.on(
+          "state_changed",
+          (snap) => {
+            const percentUploaded = Math.round(
+              (snap.bytesTransferred / snap.totalBytes) * 100
+            );
+            this.setState({ percentUploaded });
+          },
+          (err) => {
+            console.error(err);
+            this.setState({
+              errors: this.state.errors.concat(err),
+              uploadState: "error",
+              uploadTask: null,
+            });
+          },
+          () => {
+            this.state.uploadTask.snapshot.ref
+              .getDownloadURL()
+              .then((downloadUrl) => {
+                this.sendFileMessage(downloadUrl, ref, pathToUpload);
+              })
+              .catch((err) => {
+                console.error(err);
+                this.setState({
+                  errors: this.state.errors.concat(err),
+                  uploadState: "error",
+                  uploadTask: null,
+                });
+              });
+          }
+        );
+      }
+    );
+  };
+
+  sendFileMessage = (fileUrl, ref, pathToUpload) => {
+    ref
+      .child(pathToUpload)
+      .push()
+      .set(this.createMessage(fileUrl))
+      .then(() => {
+        this.setState({ uploadState: "done" });
+      })
+      .catch((err) => {
+        console.error(err);
+        this.setState({
+          errors: this.state.errors.concat(err),
+        });
+      });
+  };
+
+  render() {
+    // prettier-ignore
+    const { errors, message, loading, modal, uploadState, percentUploaded, emojiPicker } = this.state;
+
+    return (
+      <Segment className="message__form">
+        {emojiPicker && (
+          <Picker
+            set="apple"
+            title="Pick your emoji"
+            onSelect={this.handleAddEmoji}
+            className="emojipicker"
+            emoji="point_up"
+           style= {{ position: 'absolute', bottom: '113px', right: '660px' }}
+          />
+        )}
+        <Input
+          fluid
+          name="message"
+          onChange={this.handleChange}
+          onKeyDown={this.handleKeyDown}
+          value={message}
+          ref={(node) => (this.messageInputRef = node)}
+          style={{ marginBottom: "0.7em" }}
+          label={
+            <Button
+              icon={emojiPicker ? "close" : "smile"}
+              content={emojiPicker ? "Close" : null}
+              onClick={this.handleTogglePicker}
+            />
+          }
+          labelPosition="left"
+          className={
+            errors.some((error) => error.message.includes("message"))
+              ? "error"
+              : ""
+          }
+          placeholder="Write your message"
+        />
+        <Button.Group icon widths="2">
+          <Button
+            labelPosition="left"
+            color="violet"
+            onClick={this.sendMessage}
+            content="Add Reply"
+            disabled={loading}
+            icon="edit"
+          />
+          <Button
+            color="orange"
+            icon="cloud upload"
+            content="Upload Media"
+            disabled={uploadState === "uploading"}
+            onClick={this.openModal}
+            labelPosition="right"
+          />
+        </Button.Group>
+        <FileModal
+          modal={modal}
+          closeModal={this.closeModal}
+          uploadFile={this.uploadFile}
+        />
+        <ProgressBar
+          uploadState={uploadState}
+          percentUploaded={percentUploaded}
+        />
+      </Segment>
+    );
+  }
+}
+
+export default MessageForm;
