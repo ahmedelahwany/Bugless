ca0993cbdd5915f9477d7cf86b7df17411b5afa4
ahmedelahwany <ahmed.elahwany26@gmail.com>
2021-10-31 10:45:38 +0100

    Add this panel to show different parts of the app ( issues , projects ,etc.)

---
+++ src/components/SidePanel/SidePanel.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { Menu, Segment } from "semantic-ui-react";
+
+import UserPanel from "./UserPanel";
+import Issues from "./Issues";
+import Projects from "./Projects";
+import DirectMessages from "./DirectMessages";
+import Starred from "./Starred";
+
+class SidePanel extends React.Component {
+  render() {
+    const {
+      currentUser,
+      primaryColor,
+      isPrivateIssue,
+      openIssueEdit,
+      openProjectEdit,
+    } = this.props;
+
+    return (
+      <Menu
+        size="large"
+        inverted
+        fixed="left"
+        vertical
+        style={{
+          background: primaryColor,
+          height: "100vh",
+          overflowY: "scroll",
+          overflowX: "hidden",
+          fontSize: "1.1rem",
+          zIndex: 999,
+        }}
+      >
+        <UserPanel primaryColor={primaryColor} currentUser={currentUser} />
+        <Starred currentUser={currentUser} />
+
+        <Issues
+          currentUser={currentUser}
+          isPrivateIssue={isPrivateIssue}
+          openIssueEdit={openIssueEdit}
+          openProjectEdit={openProjectEdit}
+        />
+
+        <Projects currentUser={currentUser} />
+
+        <DirectMessages currentUser={currentUser} />
+      </Menu>
+    );
+  }
+}
+
+export default SidePanel;
